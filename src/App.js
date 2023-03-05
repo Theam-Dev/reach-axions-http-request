87a1381286c38efb8aa286403919e227c7624183
theam-dev <phearakroun@gmail.com>
2023-03-05 17:31:41 +0700

    Axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class PostList extends React.Component {
+  state = {
+    posts: []
+  }
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/posts`)
+      .then(res => {
+        const posts = res.data;
+        this.setState({ posts });
+      })
+  }
+  render() {
+    return (
+      <div>
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+          <div class="container">
+            <a class="navbar-brand" href="#">Tutorail Sharing</a>
+          </div>
+        </nav>
+        <div class="container">
+          <table className="table table-bordered">
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Body</th>
+            </tr>
+            {this.state.posts.map((post) => (
+              <tr>
+                <td>{post.id}</td>
+                <td>{post.title}</td>
+                <td>{post.body}</td>
+              </tr>
+            ))}
+          </table>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
